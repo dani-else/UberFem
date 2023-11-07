bd0b628cfc630f75e02d1ccc672985588706f35f
dani-else <maria.f.daniels@gmail.com>
2023-11-07 17:19:34 -0300

    commitV1.0.1

---
+++ app/src/main/java/com/danielse/uberfem/SelectOptionAuthActivity.java
@@ -0,0 +1,14 @@
+package com.danielse.uberfem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectOptionAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_option_auth);
+    }
+}
