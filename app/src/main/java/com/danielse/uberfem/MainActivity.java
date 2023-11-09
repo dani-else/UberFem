ba46f2c6707e468f0a638184a3344199d6a1ef35
dani-else <maria.f.daniels@gmail.com>
2023-11-09 15:29:08 -0300

    commitV1.0.1

--- app/src/main/java/com/danielse/uberfem/MainActivity.java
+++ app/src/main/java/com/danielse/uberfem/MainActivity.java
@@ -2,13 +2,41 @@ package com.danielse.uberfem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonSoyConductora;
+    Button buttonSoyPasajera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSoyConductora = findViewById(R.id.btnSoyConductora);
+        buttonSoyPasajera = findViewById(R.id.btnSoyPasajera);
+
+        buttonSoyConductora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSelectAuth();
+            }
+        });
+
+        buttonSoyPasajera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSelectAuth();
+            }
+        });
+    }
+
+    private void goToSelectAuth() {
+        Intent intent = new Intent(MainActivity.this, SelectOptionAuthActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/danielse/uberfem/SelectOptionAuthActivity.java
+++ app/src/main/java/com/danielse/uberfem/SelectOptionAuthActivity.java
@@ -1,14 +1,22 @@
 package com.danielse.uberfem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Seleccionar opción");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
